77ca2b7ecc49344a787c83f90d80916dc85c8129
tangyixiang <tyx851565913@163.com>
2022-12-12 12:21:41 +0800

    🎈 perf:删除获取图形验证码

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -19,7 +19,7 @@ function Login() {
   const from = location.state?.from?.pathname || '/'
 
   useEffect(() => {
-    getCaptchaCode()
+    // getCaptchaCode()
   }, [])
 
   const getCaptchaCode = async () => {
